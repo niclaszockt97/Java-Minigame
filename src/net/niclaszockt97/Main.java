6c69bba57ab2e81d9e75f512e0027bcf5d36e520
niclaszockt97 <147427588+niclaszockt97@users.noreply.github.com>
2025-12-02 20:45:56 +0100

    Add class start to create windows and create main class.

--- src/net/niclaszockt97/Main.java
+++ src/net/niclaszockt97/Main.java
@@ -1,4 +1,18 @@
 package net.niclaszockt97;
 
+import net.niclaszockt97.utility.window;
+
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame main_win = window.addFrame("Game",500,300,null, true);
+        JFrame modifier = window.addFrame("Modifier", 200, 150,null, false);
+
+        JMenu main_menu = window.createMenu("test", "TEST", true);
+        main_win.add(main_menu);
+
+        System.out.println(main_win.getRootPane().getClientProperty("id"));
+        System.out.println(modifier.getRootPane().getClientProperty("id"));
+    }
 }
--- src/net/niclaszockt97/utility/window.java
+++ src/net/niclaszockt97/utility/window.java
@@ -1,11 +1,42 @@
 package net.niclaszockt97.utility;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.UUID;
 
 public class window {
-    public static JFrame addFrame(String frame_title) {
-      JFrame frame = new JFrame(frame_title);
-      frame.add();
-     return null;
+
+    //Frame Methode.
+    public static JFrame addFrame(String frame_title, int dim_x, int dim_y, LayoutManager layout, boolean visible) {
+        // Init Frame
+        JFrame frame = new JFrame(frame_title);
+        frame.setSize(dim_x, dim_y);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(layout);
+
+        // Set unique identifier
+        String id = UUID.randomUUID().toString();
+        frame.getRootPane().putClientProperty("id", id);
+
+        frame.setVisible(visible);
+        return frame;
     }
+
+    //JMenuBar
+    public static JMenuBar createMenuBar(){
+        JMenuBar menu_bar = new JMenuBar();
+        menu_bar.createToolTip();
+
+        return menu_bar;
+    }
+
+    //MenuBar Methode
+    public static JMenu createMenu(String s, String text, boolean vis) {
+        JMenu menu = new JMenu(s);
+        menu.setText(text);
+
+        menu.setVisible(vis);
+        return menu;
+    }
+
 }
